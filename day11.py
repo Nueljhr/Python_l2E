7f27f074eab31c83680b3b8bc0d1d48dcf4453a2
sbrown <samitexb1@gmail.com>
2026-07-23 17:11:18 +0100

    completion of day11

---
+++ day11.py
@@ -0,0 +1,13 @@
+
+# def greet():
+#     print("Hello!")
+#     print("Welcome to python.")
+
+# greet()
+
+def Welcome():
+    print("Welcome")
+    print("Please log in")
+    print("Enjoy your stay")
+
+Welcome()
---
+++ day11_part2.py
@@ -0,0 +1,24 @@
+
+# def greet():
+#     print("Hello!")
+#     print("Have a wonderful day!")
+
+# greet()
+
+# print("----------")
+
+# greet()
+
+# print("----------")
+
+# greet()
+
+
+def motivate():
+    print("never stop learn, 15-minutes rule rules.")
+    print("practice every day.")
+    print("Great developers are built, not born.")
+
+motivate()
+print("----------")
+motivate()
