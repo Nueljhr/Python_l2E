f8afd44715a4279aee0f742e4ad6d03c27713a07
Nueljhr <jvrnuel@gmail.com>
2026-08-02 23:55:01 +0100

    day21 done

--- rough_work.py
+++ rough_work.py
@@ -4,4 +4,6 @@
 
 # print("supposed to be a string".upper())
 
-# += means compound assignment operator. It adds the right operand to the left operand and assigns the result to the left operand.
+# += means compound assignment operator, which adds the right operand to the left operand and assigns the result to the left operand. For example:
+# x = 5
+# x += 3  # This is equivalent to x = x + 3
