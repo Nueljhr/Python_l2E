a9d3d664c2f072775107f6cd6de851b257731a48
Nueljhr <jvrnuel@gmail.com>
2026-08-21 01:15:56 +0100

    upper room ass done

---
+++ upper_room_exe.py
@@ -0,0 +1,60 @@
+# USE F-STRINGS TO PRINT A SENTANCE WITH VARIABLES
+
+# name = "Nuel"
+# hobby = "coding"
+# print(f"Hi, my name is {name}, i love {hobby} for fun.")
+
+# USERNAME GENERATOR
+# from first and last name
+
+# class username:
+#     pass
+
+# Username = username()
+
+# Username.First = input("What is your firstname: ")
+# Username.Last = input("What is your lastname: ")
+
+# print(Username.First)
+# print(Username.Last)
+
+# PRACTICE STRING SLICING
+# word = "individual"
+
+# print(word[6:])
+
+# this will display just dual
+
+# RECTANGLE CALCULATOR (AREA AND PERIMETER)
+
+# len_rect = float(input("Enter length of rectangle: "))
+
+# wid_rect = float(input("Enter width of rectangle: "))
+# # calculate area
+# area = len_rect * wid_rect
+# # calculate perimeter
+# perimeter = 2 * (len_rect + wid_rect)
+
+# print(f"Area: {area}")
+# print(f"Perimeter: {perimeter}")
+
+# BMI CALCULATOR
+
+# weight = float(input("Enter your weight: "))
+# height = float(input("Enter your height: "))
+
+# BMI = weight / (height * height)
+# print(int(BMI))
+
+
+
+
+# def bmi():
+#     weight = float(input("Enter your weight: "))
+#     height = float(input("Enter your height: "))
+    
+#     return weight height
+
+
+    
+
