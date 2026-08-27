512585b7da6c63cc72b5f6fba166a7806498b70e
Nueljhr <jvrnuel@gmail.com>
2026-08-27 06:33:34 +0100

    new day starting

--- day33.py
+++ day33.py
@@ -34,34 +34,61 @@
 
 
 
-class Pet:
+# class Pet:
 
-    def __init__(self, name, species, hunger):
+#     def __init__(self, name, species, hunger):
+#         self.name = name
+#         self.species = species
+#         self.hunger = hunger
+
+#     def introduce(self):
+#         print(f" My name is {self.name} and I am a/an {self.species}.")
+
+#     def eat(self):
+#         if self.hunger >= 40:
+#             self.hunger -= 20
+
+#         return self.hunger
+
+# pet1 = Pet("Kultur", "Human", 25)
+# pet2 = Pet("Munich", "AI", 50)
+# pet3 = Pet(name="Claudiadaves", species="AI", hunger=45)
+
+# print("===== Using __init__ =====")
+# print()
+# pet1.introduce()
+# print(pet1.eat())
+# print()
+# pet2.introduce()
+# print(pet2.eat())
+# print()
+# pet3.introduce()
+# print(pet3.eat())
+# print("==== THANKS ====")
+
+
+#STUDENT MGT SYSTEM
+
+class Student:
+    
+    def __init__(self, name, age, course, score):
         self.name = name
-        self.species = species
-        self.hunger = hunger
-
+        self.age = age
+        self.course = course
+        self.score = score
+        
     def introduce(self):
-        print(f" My name is {self.name} and I am a/an {self.species}.")
-
-    def eat(self):
-        if self.hunger >= 40:
-            self.hunger -= 20
-
-        return self.hunger
-
-pet1 = Pet("Kultur", "Human", 25)
-pet2 = Pet("Munich", "AI", 50)
-pet3 = Pet(name="Claudiadaves", species="AI", hunger=45)
+        print(f"My name is {self.name}, I am {self.age} years old, I love studing {self.course} and my last score was {self.score}.")
+        
+        
+student1 = Student("Munich", 8, "Assembly", 90)
+student2 = Student("Kultur", 26, "Python", 95)
+student3 = Student(name="Nuel", age=27, course="Golang", score=98)
 
 print("===== Using __init__ =====")
 print()
-pet1.introduce()
-print(pet1.eat())
+student1.introduce()
 print()
-pet2.introduce()
-print(pet2.eat())
+student2.introduce()
 print()
-pet3.introduce()
-print(pet3.eat())
-print("==== THANKS ====")
+student3.introduce()
